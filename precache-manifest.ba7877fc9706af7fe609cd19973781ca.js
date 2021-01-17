39169699f78a3372b1fb1612b174fd539c04b9c2
Relise228 <andreysmit43@gmail.com>
2021-01-17 13:07:12 +0200

    Updates

---
+++ precache-manifest.ba7877fc9706af7fe609cd19973781ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40854e975333fc4cb48f62a565e8fe2f",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "4179923186eb70154cee",
+    "url": "/social-network/static/css/3.de0afdb1.chunk.css"
+  },
+  {
+    "revision": "54c96a8bbb573464d7aa",
+    "url": "/social-network/static/css/4.908eaaf8.chunk.css"
+  },
+  {
+    "revision": "bcaf262d52444337fb8a",
+    "url": "/social-network/static/css/main.a34f786f.chunk.css"
+  },
+  {
+    "revision": "cdeafa0d8dd607673584",
+    "url": "/social-network/static/js/2.296c74f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.296c74f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4179923186eb70154cee",
+    "url": "/social-network/static/js/3.8aa58695.chunk.js"
+  },
+  {
+    "revision": "54c96a8bbb573464d7aa",
+    "url": "/social-network/static/js/4.a7750237.chunk.js"
+  },
+  {
+    "revision": "bcaf262d52444337fb8a",
+    "url": "/social-network/static/js/main.e2a5f9ba.chunk.js"
+  },
+  {
+    "revision": "0cd40ac839faf86c0c4b",
+    "url": "/social-network/static/js/runtime-main.a1f0995f.js"
+  }
+]);
